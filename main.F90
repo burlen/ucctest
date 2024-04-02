11d003bd51f0d70f52e2eea034e9f38de489d4fc
Burlen Loring <bloring@nvidia.com>
2024-04-02 08:41:27 -0700

    minified main

--- main.F90
+++ main.F90
@@ -1,10 +1,7 @@
-
-
-
 program main
   use mpi
   implicit none
-  integer ierr, nProcs, procId, req, recv, msg, cnt, tot, i
+  integer ierr, nProcs, procId, recv, cnt, tot, i
   real(4), allocatable ::  sbuf(:), rbuf(:)
 
   call MPI_Init_thread(MPI_THREAD_MULTIPLE, recv, ierr)
@@ -12,14 +9,12 @@ program main
   call MPI_Comm_size(MPI_COMM_WORLD, nProcs, ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, procId, ierr)
 
-  msg = 32
-  cnt = msg / 4
+  cnt = 8
   tot = nProcs*cnt
 
   write(*,'(I0,A,I0,A,I0)')nProcs,' procs cnt ',cnt,' tot ',tot
 
   allocate(sbuf(1:tot), rbuf(1:tot))
-
   !$omp target data map(alloc:sbuf, rbuf)
 
   !$omp target teams distribute parallel do map(alloc:sbuf)
@@ -35,8 +30,6 @@ program main
   write(*,*) rbuf(:)
 
   !$omp end target data
-
   call MPI_Finalize(ierr)
-
 
 end program
